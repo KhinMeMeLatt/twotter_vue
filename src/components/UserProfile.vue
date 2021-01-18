125cbc67150bc9341d99ae53e2ed331b355b9935
Khin Me Me Latt <khinmemelatt@outlook.com>
2021-01-18 23:07:11 +0630

    Dynamic Styling, SCSS, & Global Styles

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -7,22 +7,24 @@
       <div class="user-profile__follower-count">
         <strong>followers: </strong>{{ followers }}
       </div>
-      <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot">
+      <form class="user-profile__create-twoot" @submit.prevent="createNewTwoot" :class="{ '--exceeded' : newTwootCharacterCount > 180 }">
         <label for="newTwoot"><strong>New Twoot</strong></label>
-        <textarea id="newTwoot" rows="4" v-model="newTwootContent"/>
+        <textarea id="newTwoot" rows="4" v-model="newTwootContent" />
 
         <div class="user-profile__create-twoot-type">
-          <label for="newTwootType"><strong>Type: </strong></label>
+          <label for="newTwootType"><strong>Type: </strong>({{ newTwootCharacterCount }}/180)</label>
           <select id="newTwootType" v-model="selectedTwootType">
-            <option :value="option.value" v-for="(option, index) in twootTypes" :key="index">
+            <option
+              :value="option.value"
+              v-for="(option, index) in twootTypes"
+              :key="index"
+            >
               {{ option.name }}
             </option>
           </select>
         </div>
 
-        <button>
-          Twoot!
-        </button>
+        <button>Twoot!</button>
       </form>
     </div>
     <div class="user-profile__twoots-wrapper">
@@ -38,18 +40,18 @@
 </template>
 
 <script>
-import TwootItem from "./TwootItem"
+import TwootItem from "./TwootItem";
 
 export default {
   name: "UserProfile",
-  components: {TwootItem},
+  components: { TwootItem },
   data() {
     return {
-      newTwootContent: '',
-      selectedTwootType: 'instant',
+      newTwootContent: "",
+      selectedTwootType: "instant",
       twootTypes: [
-        { value: 'draft', name: 'Draft' },
-        { value: 'instant', name: 'Instant Twoot'}
+        { value: "draft", name: "Draft" },
+        { value: "instant", name: "Instant Twoot" },
       ],
       followers: 0,
       user: {
@@ -77,23 +79,26 @@ export default {
     fullName() {
       return "${this.user.firstName} ${this.user.lastName}";
     },
+    newTwootCharacterCount() {
+      return this.newTwootContent.length;
+    }
   },
   methods: {
     followUser() {
       this.followers++;
     },
     toggleFavourite(id) {
-      console.log("Favourited Tweet #${id}"+id);
+      console.log("Favourited Tweet #${id}" + id);
     },
     createNewTwoot() {
-      if(this.newTwootContent && this.selectedTwootType !== 'draft') {
+      if (this.newTwootContent && this.selectedTwootType !== "draft") {
         this.user.twoots.unshift({
           id: this.user.twoots.length + 1,
-          content: this.newTwootContent
-        })
-        this.newTwootContent = '';
+          content: this.newTwootContent,
+        });
+        this.newTwootContent = "";
       }
-    }
+    },
   },
   mounted() {
     this.followUser();
@@ -101,45 +106,55 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
 .user-profile {
   display: grid;
   grid-template-columns: 1fr 3fr;
   width: 100%;
   padding: 50px 5%;
-}
 
-.user-profile__user-panel {
-  display: flex;
-  flex-direction: column;
-  margin-right: 50px;
-  padding: 20px;
-  background-color: white;
-  border-radius: 5px;
-  border: 1px solid #dfe3e8;
-}
+  .user-profile__user-panel {
+    display: flex;
+    flex-direction: column;
+    margin-right: 50px;
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid #dfe3e8;
 
-.user-profile__admin-badge {
-  background: rebeccapurple;
-  color: white;
-  border-radius: 5px;
-  margin-right: auto;
-  padding: 0 10px;
-  font-weight: bold;
-}
+    h1 {
+      margin: 0;
+    }
 
-h1 {
-  margin: 0;
-}
+    .user-profile__admin-badge {
+      background: rebeccapurple;
+      color: white;
+      border-radius: 5px;
+      margin-right: auto;
+      padding: 0 10px;
+      font-weight: bold;
+    }
 
-.user-profile__twoots-wrapper {
-  display: grid;
-  grid-gap: 10px;
-}
+    .user-profile__create-twoot {
+      padding-top: 20px;
+      display: flex;
+      flex-direction: column;
 
-.user-profile__create-twoot {
-  padding-top: 20px;
-  display: flex;
-  flex-direction: column;
+      &.--exceeded {
+        color: red;
+        border-color: red;
+
+        button {
+          background-color: red;
+          border: none;
+          color: white;
+        }
+      }
+    }
+  }
+  .user-profile__twoots-wrapper {
+    display: grid;
+    grid-gap: 10px;
+  }
 }
 </style>
